ea05d4494af8c800f031f04bb6a42f475a110ba3
sky <ismatch@qq.com>
2018-09-17 23:37:15 +0800

    study vue-cli

---
+++ Vue-Cli/vue-playlist/src/App.vue
@@ -0,0 +1,31 @@
+<!--1.模板：html结构-->
+<template>
+  <div id="app">
+   <h2>{{ title }}</h2>
+   <users></users>
+  </div>
+</template>
+
+<!--2.行为：处理逻辑-->
+<script>
+// 局部注册组件
+import Users from './components/Users'
+
+
+export default {
+  name: 'App',
+  data:function(){
+    return {
+      title:'脚手架项目学习中...'
+    }
+  },
+  components:{
+    'users':Users
+  }
+}
+</script>
+
+<!-- 3.样式：解决样式 -->
+<style>
+
+</style>
---
+++ Vue-Cli/vue-playlist/src/components/Users.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="users">
+   <ul>
+       <li v-for="user in users">
+           <p>
+               {{ user }}
+           </p>
+       </li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  data () {
+    return {
+      users: ['张三','李四','王五','赵六']
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

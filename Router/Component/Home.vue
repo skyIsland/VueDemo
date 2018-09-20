fc0383d37b9d4f6cc6aa8955d3f69eab141a73ab
sky <ismatch@qq.com>
2018-09-21 00:24:36 +0800

    hi

---
+++ Router/Component/Home.vue
@@ -0,0 +1,14 @@
+export default{
+    computed:{
+        username(){
+            return this.$route.params.username
+        }
+    },
+    methods:{
+        goBack(){
+            window.history.length > 1
+                ? this.$router.go(-1)
+                : this.$router.push('/')
+        }
+    }
+}
